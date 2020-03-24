f03a8220747c938be031003420eb66fbf3e9e0b3
Ben Villiere <benoit.villiere@gmail.com>
2020-03-24 23:10:52 +0000

    Added clear test

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
 import { useLocalStorage } from './'
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks"
 
 const SAMPLE_OBJECT = {
     title: 'An article about the meaning of life',
@@ -14,69 +14,83 @@ describe('useLocalStorage', () => {
     it('can save an object to local storage', () => {
         const { result } = renderHook(() => (
             useLocalStorage('draft', SAMPLE_OBJECT)
-        ));
+        ))
 
-        expect(result.current[0].title).toBe('An article about the meaning of life');
+        expect(result.current[0].title).toBe('An article about the meaning of life')
     })
 
     it('can update an object from local storage', () => {
         const { result } = renderHook(() => (
             useLocalStorage('draft', SAMPLE_OBJECT)
-        ));
+        ))
 
-        expect(result.current[0].title).toBe('An article about the meaning of life');
+        expect(result.current[0].title).toBe('An article about the meaning of life')
 
         act(() => {
             result.current[1]({
                 ...result.current[0],
                 title: 'Changed title'
-            });
-        });
+            })
+        })
 
-        expect(result.current[0].title).toBe('Changed title');
+        expect(result.current[0].title).toBe('Changed title')
     })
 
     it('can save a number to local storage', () => {
         const { result } = renderHook(() => (
             useLocalStorage('number', SAMPLE_NUMBER)
-        ));
+        ))
 
-        expect(result.current[0]).toBe(SAMPLE_NUMBER);
+        expect(result.current[0]).toBe(SAMPLE_NUMBER)
     })
 
     it('can update a number from local storage', () => {
         const { result } = renderHook(() => (
             useLocalStorage('number', SAMPLE_NUMBER)
-        ));
+        ))
 
-        expect(result.current[0]).toBe(SAMPLE_NUMBER);
+        expect(result.current[0]).toBe(SAMPLE_NUMBER)
 
         act(() => {
-            result.current[1](SAMPLE_NUMBER * 2);
-        });
+            result.current[1](SAMPLE_NUMBER * 2)
+        })
 
-        expect(result.current[0]).toBe(SAMPLE_NUMBER * 2);
+        expect(result.current[0]).toBe(SAMPLE_NUMBER * 2)
     })
 
     it('can save a string to local storage', () => {
         const { result } = renderHook(() => (
             useLocalStorage('string', SAMPLE_STRING)
-        ));
+        ))
 
-        expect(result.current[0]).toBe(SAMPLE_STRING);
+        expect(result.current[0]).toBe(SAMPLE_STRING)
     })
 
     it('can update a number from local storage', () => {
         const { result } = renderHook(() => (
             useLocalStorage('string', SAMPLE_STRING)
-        ));
+        ))
 
-        expect(result.current[0]).toBe(SAMPLE_STRING);
+        expect(result.current[0]).toBe(SAMPLE_STRING)
 
         act(() => {
-            result.current[1]('Nope.');
-        });
+            result.current[1]('Nope.')
+        })
 
-        expect(result.current[0]).toBe('Nope.');
+        expect(result.current[0]).toBe('Nope.')
+    })
+
+    it('can save data to local storage and clear itself', () => {
+        const { result } = renderHook(() => (
+            useLocalStorage('string', SAMPLE_STRING)
+        ))
+
+        expect(result.current[0]).toBe(SAMPLE_STRING)
+
+        act(() => {
+            result.current[1](null)
+        })
+
+        expect(result.current[0]).toBeNull()
     })
 })
